881185ab6d46f9307df36ff3aace31dc4bc40a84
951kiwi <aimu.shibahara@gmail.com>
2023-07-01 17:00:26 +0900

    change_button

--- test_darts/js/button.js
+++ test_darts/js/button.js
@@ -1,7 +1,20 @@
 const startbutton = document.getElementById('startbutton');
-
+const settingbutton = document.getElementById('settingbutton');
+const scorebutton = document.getElementById('scorebutton');
+const nbutton = document.getElementById('nbutton');
 const divClick = () => {
   alert('ゲームがスタートされます');
 };
-
+const settingClick = () => {
+  alert('設定画面に移動します');
+};
+const nClick = () => {
+  alert('？？？がスタートされます');
+};
+const scoreClick = () => {
+  alert('スコアが表示されます');
+};
 startbutton.addEventListener('click', divClick);
+settingbutton.addEventListener('click', settingClick);
+nbutton.addEventListener('click', nClick);
+scorebutton.addEventListener('click', scoreClick);
